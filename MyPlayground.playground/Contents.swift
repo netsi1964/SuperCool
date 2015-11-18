cd6acdf7bb892020cea91353f5cd9485558b4fe9
Sten Hougaard <netsi1964@gmail.com>
2015-11-18 10:26:51 +0100

    Hey! A Playground!

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Excercise 2
+func add(numberA:Double, numberB:Double) -> Double {
+    return numberA+numberB
+}
+
+add(100, numberB:100)
+
+
+// Excercise 3
+func subtract(numberA:Int, numberB:Int) -> Int {
+    return numberA-numberB
+}
+
+subtract(100, numberB:1100)
+
+// Excercise 4
+func multiply(numberA:Float, numberB:Float) -> Float {
+    return numberA * numberB
+}
+
+multiply(100, numberB:1100)
+
+// Excercise 5
+func divide(numberA:Double, numberB:Double) -> Double {
+    return numberA / numberB
+}
+
+divide(100, numberB:1100)
